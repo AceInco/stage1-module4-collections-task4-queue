9d380965c41ee69cc8c00ee2041d0cd9761dda49
Mikita <vishnevkiy.nikita@gmail.com>
2023-09-26 22:12:53 +0300

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,21 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+        while(!firstQueue.isEmpty() || !secondQueue.isEmpty()){
+            firstQueue.offer(deque.removeLast());
+            deque.add(firstQueue.remove());
+            deque.add(firstQueue.remove());
+            if(!secondQueue.isEmpty()){
+                secondQueue.offer(deque.removeLast());
+                deque.add(secondQueue.remove());
+                deque.add(secondQueue.remove());
+            }
+        }
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            linkedList.addLast(i);
+        }
+        int i = everyDishNumberToEat - 1;
+        while(list.size() != numberOfDishes){
+            list.add(linkedList.remove(i));
+            i += everyDishNumberToEat - 1;
+            if(i >= linkedList.size()){
+                i -= linkedList.size();
+            }
+            if(list.size() == numberOfDishes - 1){
+                list.add(linkedList.remove());
+            }
+        }
+        return list;
     }
 }
